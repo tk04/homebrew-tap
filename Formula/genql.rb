30b968b3c5a22e798e52e16cd8e329d872e52eba
taloufi <taloufi@iu.edu>
2023-01-03 14:20:37 -0500

    adjusted rb syntax

--- Formula/genql.rb
+++ Formula/genql.rb
@@ -11,10 +11,12 @@ class Genql < Formula
     elsif Hardware::CPU.avx2?
       url "https://github.com/tk04/genql/releases/download/#{version}/genql-darwin-amd64.zip"
       sha256 "10c49f873f37eb7fbc40230320eebaa77d5fbb6c227c613e5e75262542575a43" 
+    end
   elsif os.linux?
     if hardware::cpu.avx2?
       url "https://github.com/tk04/genql/releases/download/#{version}/genql-linux-amd64.zip"
       sha256 "889237a3337f13ed8d7b0f280bf53317aca63c2632be0667ce649d939b0cfda5" 
+    end
   else
     odie "Unsupported platform"
   end
